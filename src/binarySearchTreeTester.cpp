fd87f4700adb45813f9fb089ceea648635649dfd
mbs <mstavola9@gmail.com>
2014-04-29 17:35:03 -0400

    Fixed

--- src/binarySearchTreeTester.cpp
+++ src/binarySearchTreeTester.cpp
@@ -9,8 +9,9 @@ int main() {
 	cout << "Please enter a number to add to a tree (-999 to quit): " << endl;
 	cin >> uInput;
 	while (uInput != -999){
-		cout << "Please enter a number to add to a tree (-999 to quit): " << endl;
 		TreeRoot.insert(uInput);
+		cout << "Please enter a number to add to a tree (-999 to quit): " << endl;
+		cin >> uInput;
 	}
 
 	cout << "In-order traversal of the tree:" << endl;
@@ -22,15 +23,17 @@ int main() {
 	cout << "Please enter a number to delete from a tree (-999 to quit): " << endl;
 	cin >> uInput;
 	while (uInput != -999){
-		cout << "Please enter a number to delete from a tree (-999 to quit): " << endl;
 		TreeRoot.deleteNode(uInput);
+		cout << "Please enter a number to delete from a tree (-999 to quit): " << endl;
+		cin >> uInput;
 	}
 
 	cout << "Please enter a number to delete from a tree (-999 to quit): " << endl;
 	cin >> uInput;
 	while (uInput != -999){
-		cout << "Please enter a number to delete from a tree (-999 to quit): " << endl;
 		TreeRoot.search(uInput);
+		cout << "Please enter a number to delete from a tree (-999 to quit): " << endl;
+		cin >> uInput;
 	}
 
 	cout << "In-order traversal of the tree:" << endl;
