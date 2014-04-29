dde3fbb25fefea6bd5143bf9475e330a544640de
mbs <mstavola9@gmail.com>
2014-04-29 17:27:48 -0400

    Finished, needs testing

--- src/binarySearchTreeTester.cpp
+++ src/binarySearchTreeTester.cpp
@@ -1,15 +1,39 @@
 #include <iostream>
 #include "binarySearchTree.h"
-
 using namespace std;
 
-int main() {
-	bSearchTreeType<int> TreeRoot = new binarySearchTreeType<int>();
-	int[] treeFill = {1, 2, 3, 4, 5, 6, 7 ,8 ,9, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-	for(int i = 0; i < 18; i++){
-		TreeRoot.insert(treeFill[i]);
-	}
-	cout << TreeRoot.nodeCount() << endl;
-	cout << TreeRoot.leavesCount();
+int main() { 
+	bSearchTreeType<int> TreeRoot;
+	int uinput;
 
+	cout << "Please enter a number to add to a tree (-999 to quit): " << endl;
+	cin >> uInput;
+	while (uInput != -999){
+		cout << "Please enter a number to add to a tree (-999 to quit): " << endl;
+		TreeRoot.insert(uInput);
+	}
+
+	cout << "In-order traversal of the tree:" << endl;
+	TreeRoot.inorderTraversal(); 
+	cout << endl;
+	cout << "Number of nodes: " << TreeRoot.treeNodeCount() << endl; 
+	cout << "Number of leaves: " << TreeRoot.treeLeavesCount() << endl; 
+
+	cout << "Please enter a number to delete from a tree (-999 to quit): " << endl;
+	cin >> uInput;
+	while (uInput != -999){
+		cout << "Please enter a number to delete from a tree (-999 to quit): " << endl;
+		TreeRoot.deleteNode(uInput);
+	}
+
+	cout << "Please enter a number to delete from a tree (-999 to quit): " << endl;
+	cin >> uInput;
+	while (uInput != -999){
+		cout << "Please enter a number to delete from a tree (-999 to quit): " << endl;
+		TreeRoot.search(uInput);
+	}
+
+	cout << "In-order traversal of the tree:" << endl;
+	TreeRoot.inorderTraversal();
+	cin.get(); 
 }
